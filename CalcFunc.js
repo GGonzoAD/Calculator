fa2b063a25751b75e8dfe82e564ab6c4673c7779
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-07-03 12:18:57 -0700

    message

--- CalcFunc.js
+++ CalcFunc.js
@@ -1,15 +1,21 @@
-function Add(){
-
+var Num1 = prompt("Please Enter a Number!");
+var Num2 = prompt("Please enter another number.");
+function Add(Num1, Num2){
+ Answer = Num1 + Num2;
+ console.log(Answer);
 }
 
-function Subtract(){
-
+function Subtract(Num1, Num2){
+    Answer = Num1 - Num2;
+    console.log(Answer);
 }
 
-function Multiply(){
-
+function Multiply(Num1, Num2){
+    Answer = Num1 * Num2;
+    console.log(Answer);
 }
 
-function Divide(){
-
+function Divide(Num1, Num2){
+    Answer = Num1 % Num2;
+    console.log(Answer);
 }
