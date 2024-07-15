19ad3700c16b82afb2cf4ca183a5b102f03e556f
GGonzoAD <158357205+GGonzoAD@users.noreply.github.com>
2024-07-15 09:35:28 -0700

    message

--- CalcFunc.js
+++ CalcFunc.js
@@ -1,30 +1,38 @@
 var Num1;
 var Num2;
-let Operator;  
-function Add(Num1, Num2){
+var Operator;  
+function Add(Num1, Num2, Operator){
  Answer = Num1 + Num2;
+ Operator = "+";
  console.log(Answer);
 }
 
-function Subtract(Num1, Num2){
+function Subtract(Num1, Num2, Operator){
     Answer = Num1 - Num2;
+    Operator = "-";
     console.log(Answer);
 }
 
-function Multiply(Num1, Num2){
+function Multiply(Num1, Num2, Operator){
     Answer = Num1 * Num2;
+    Operator = "*";
     console.log(Answer);
 }
 
-function Divide(Num1, Num2){
+function Divide(Num1, Num2, Operator){
     Answer = Num1 / Num2;
+    Operator = "/";
     console.log(Answer);
 }
 
-Add(Num1, Num2);
 
-Subtract(Num1, Num2);
 
-Multiply(Num1, Num2);
+function Operate(Num1, Num2, Operator){
+    Add(Num1, Num2, Operator);
 
-Divide(Num1, Num2);
+    Subtract(Num1, Num2, Operator);
+
+    Multiply(Num1, Num2, Operator);
+
+    Divide(Num1, Num2, Operator);
+}
